fd83544898c62290ec56e91a9d79f497bb57e871
Philippe Gagnon <philgagnon12@gmail.com>
2022-10-26 22:03:24 -0400

    Windows vimrc

--- cmake/vim.cmake
+++ cmake/vim.cmake
@@ -3,7 +3,7 @@ file( COPY sensible.vim DESTINATION ${CMAKE_INSTALL_PREFIX} )
 file( COPY base16-shell.vim DESTINATION ${CMAKE_INSTALL_PREFIX} )
 
 install( FILES ${CMAKE_CURRENT_BINARY_DIR}/_vimrc
-	DESTINATION	$ENV{HOME}
-	RENAME		.vimrc
+	DESTINATION	$<IF:$<BOOL:${WIN32}>,$ENV{LOCALAPPDATA}/nvim,$ENV{HOME}>
+	RENAME		$<IF:$<BOOL:${WIN32}>,init.vim,.vimrc> # init.vim(neovim)
 )
 
